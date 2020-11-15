8cc590f684567d4d149946eec28cc76fe29334f9
GKN <835984026@qq.com>
2020-11-15 23:41:19 +0800

    test key drop

--- src/config.js
+++ src/config.js
@@ -1,5 +1,20 @@
 // 游戏设置
 export default {
+  
   width: 667,
-  height: 375
+  height: 375,
+  
+  // 游戏设置
+  game: {
+    // 键位Y轴的运动距离
+    keyDistanceY: 375,
+    // 顶部和底部的比例
+    topRatio: 0.15,
+    // 底部宽度
+    bottomWidth: 600,
+    // 键位的初速度(1速)，几速乘以几
+    keyMoveSpeedInitial: 0,
+    // 键位从顶部到底部用时(1速)，几速除以几
+    keyMoveTime: 2500
+  }
 }
---
+++ src/game/keys/TapKey.js
@@ -0,0 +1,11 @@
+import { Sprite } from 'pixi.js'
+
+import config from '@/config.js'
+
+class TapKey extends Sprite {
+  constructor () {
+    
+  }
+  
+  
+}
